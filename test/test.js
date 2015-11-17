40a7d616fa1879853825bb8e970eea3369224b8f
Daniel <danielgeri@gmail.com>
2015-11-17 13:35:00 -0800

    - testing mocha

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var supertest = require('supertest');
+var should = require('should');
+
+describe('Routing',function() {
+		it('should pass', function(done) {
+			done();
+		});
+
+		it('should pass again', function(done) {
+			done();
+		})
+})
